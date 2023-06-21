7f3fadd08e367afd2e825c5e2c940f1d33627957
Mateusz Jasiński <mateusz.jasinski02@student.wat.edu.pl>
2023-06-21 14:43:38 +0200

    First commit

---
+++ app/src/main/java/com/example/am_anime_list/SearchingActivity.java
@@ -0,0 +1,14 @@
+package com.example.am_anime_list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searching);
+    }
+}
